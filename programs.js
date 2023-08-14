0ac3e304677df0d5d37c5ca39e64047b153fc701
“prasanna kumar” <prasanna.kumar@codecraft.co.in>
2023-08-14 15:34:35 +0530

    feat:Implemented Even number

--- programs.js
+++ programs.js
@@ -1,1 +1,25 @@
 //this file contains logical programs ragarding numbers....
+/**
+ * 
+ * @param {a} number 
+ * @param {b} number
+ * @returns return even number
+ */
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+let a = 6;
+let b = 9;
+
+if (isEven(a)) {
+  console.log(`${a} is even.`);
+} else {
+  console.log(`${a} is not even.`);
+}
+
+if (isEven(b)) {
+  console.log(`${b} is even.`);
+} else {
+  console.log(`${b} is not even.`);
+}
